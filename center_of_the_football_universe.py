bf62270dcb9925cfd3ede043851335c1905e5cbf
escosio <scottd26@gmail.com>
2022-10-12 23:49:02 -0400

    support bye weeks

--- center_of_the_football_universe.py
+++ center_of_the_football_universe.py
@@ -16,10 +16,13 @@ def get_venue_dict():
     dict = {}
     print("Week", week)
     for game in range(0,16):
-        teams = response.json()["events"][game]["name"].split(" at ")
-        venue = response.json()["events"][game]["competitions"][0]["venue"]["fullName"]
-        dict[teams[0]]=venue
-        dict[teams[1]]=venue
+        try:
+            teams = response.json()["events"][game]["name"].split(" at ")
+            venue = response.json()["events"][game]["competitions"][0]["venue"]["fullName"]
+            dict[teams[0]]=venue
+            dict[teams[1]]=venue
+        except:
+            pass
     return dict 
 
 def met_life(dict):
@@ -27,33 +30,11 @@ def met_life(dict):
         if venue == 'MetLife Stadium':
             print(team)
 
-# don't need this anymore
-def is_international(week,team):
-    is_international = bool()
-    #  week 4 vikings saints
-    if week == 4 and team in ("Minnesota Vikings","New Orleans Saints"):
-        is_international = True
-    # week 5 giants packers
-    elif week == 5 and team in ("New York Giants","Green Bay Packers"):
-        is_international = True
-    # week 8 broncos jags
-    elif week == 8 and team in ("Denver Broncos","Jacksonville Jaguars"):
-        is_international = True
-    # week 10 seahawks bucs
-    elif week == 10 and team in ("Seattle Seahawks","Tampa Bay Buccaneers"):
-        is_international = True
-    # week 11 49ers Cardinals 
-    elif week == 11 and team in ("San Francisco 49ers","Arizona Cardinals"):
-        is_international = True
-    else:
-        is_international = False
-    return is_international
-
-def get_penalty(team, yards, lookup_venue):
-    venue = lookup_venue[team]
+def get_penalty(team_name, yards, lookup_venue):
+    venue = lookup_venue[team_name]
     penalty = float(venue_penalties[venue])
-    print("The ", team, "are playing at", venue, "and the penalty is", penalty)
-    print("penalty is", penalty)
+    print("The ", team_name, "are playing at", venue, "and the penalty is", penalty)
+    # print("penalty is", penalty)
     print("Week score:", round(yards - penalty, 2))
     
 def find_team(team, dict):
@@ -81,9 +62,13 @@ if __name__ == '__main__':
     print("Welcome to the CENTER OF THE FOOTBALL UNIVERSE","\n")
     weekly_venues = get_venue_dict()
     for player in weekly_plays:
-        team = find_team(weekly_plays[player].get("team"), weekly_venues)
-        print(player, "plays", team)
-        get_penalty(team, weekly_plays[player].get("yards"),weekly_venues)
-        print("\n")
+        team_name = str(weekly_plays[player].get("team"))
+        team = find_team(team_name.title(), weekly_venues)
+        print(player, "plays", team_name)
+        if team in list(weekly_venues.keys()):
+            get_penalty(team, weekly_plays[player].get("yards"),weekly_venues)
+            print("\n")
+        else:
+            print(f"Could not find a game for the {team_name} this week. Do they have a bye?", '\n')
 
 
